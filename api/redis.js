3061888b231ba9060ab02932dd30a253d72d99a2
DILG Programmer <juderibleza36@gmail.com>
2024-05-24 17:05:19 +0800

    new

--- api/redis.js
+++
@@ -1,6 +0,0 @@
-// import { Redis } from "ioredis";
-
-// export const redis  = new Redis({
-//     url: process.env.UPSTASH_REDIS_REST_URL,
-//     token: process.env.UPSTASH_REDIS_REST_TOKEN
-// })

c642a226728b914cd7c9e0bb4c2aa33dd0bfa8dc
DILG Programmer <juderibleza36@gmail.com>
2024-08-06 15:11:52 +0800

    Checked

--- api/axios.tsx
+++ api/axios.tsx
@@ -2,7 +2,7 @@ import axios from "axios"
 const dev = "http://localhost:3000"
 const production = "https://audit-tracker-admin.firebaseapp.com/"
 export default axios.create({
-    baseURL: dev,
+    baseURL: production,
     headers: {
         "Content-Type": "application/json",
       },
